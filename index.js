ee38282ebf0c2d5e6a7a592016d20f58df224222
Marco Israel Silicaro <marcosilicaro@gmail.com>
2021-02-06 09:32:13 -0800

    mongodb server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const {ApolloServer} = require('apollo-server')
+const mongoose = require('mongoose')
+const gql = require('graphql-tag')
+const {connection_url} = require('./config')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers= require('./graphql/resolvers')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose
+  .connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true } )
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err)
+  })
